732bd1ebf6f026e4c981c0197e5767b7309cc26d
Casas <atcasas@student.uiwtx.edu>
2022-01-26 13:14:57 -0600

    Ranged Zombie now tosses a projectile, projectile needs to be added

---
+++ Assets/prefabs/Zombie/Zombie_Ranged/RangedZombie.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedZombie : Zombie
+{
+    public override void AttackPoint()
+    {
+        base.AttackPoint();
+
+        Debug.Log("*trows projectile*");
+
+    }
+}
